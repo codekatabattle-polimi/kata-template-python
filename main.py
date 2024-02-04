95b3805ac48fd3b3495e61754807b347a1bf9373
Giovanni Orciuolo <giovanni.orciuolo1999@gmail.com>
2024-02-04 18:21:34 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def kataSolution(input):
+    # Your solution code here
+    return ""
+
+def main():
+    # Read input from file
+    with open('input.txt', 'r') as file:
+        input = file.read()
+
+    # Call kataSolution function
+    output = kataSolution(input)
+
+    # Print output to standard output
+    print(output)
+
+if __name__ == '__main__':
+    main()
